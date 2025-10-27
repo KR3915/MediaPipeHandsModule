2e49662c774c78a8feb2635ff76d7d45a15deb07
kr3915 <ondrej.brdek@gmail.com>
2025-10-27 21:25:28 +0100

    mp hands bug fixed

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -16,14 +16,13 @@ class handDetector():
         self.mpDraw = mp.solutions.drawing_utils
 
     def find_hands(self, img, draw=True):
-         if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:        
-               imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #convert to rgb
-               self.results = self.hands.process(imgRGB) #get landmarks from image
-               self.mpDraw.draw_landmarks(img, handLms, self.mp_hands.HAND_CONNECTIONS)
-               return img
-         else:
-            return 0
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mp_hands.HAND_CONNECTIONS)
+        return img
 
     def find_position(self, img, hand_no=0, draw=True):
 
@@ -34,12 +33,10 @@ class handDetector():
                 for id, lm in enumerate(handLms.landmark):  
                     h, w, c = img.shape # get shape of image (height, width, color channel)
                     cx, cy = int(lm.x*w), int(lm.y*h) #get x and y of landmark in pixels
-                    lm_list.append[id, cx, cy]
+                    lm_list.append([id, cx, cy])
                     #draws on the landmark 0
-                    if draw:
-                        cv2.circle(img, (cx, cy), 28, (255,0,255), cv2.FILLED) #draw circle at landmark 0 (img, (x, y, radius_px, (R,G,B), type)
 
-                self.mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS) 
+                self.mpDraw.draw_landmarks(img, my_hand, self.mp_hands.HAND_CONNECTIONS) 
         return lm_list 
 
     # cTime = time.time()
@@ -59,16 +56,17 @@ def main():
     while True:
         success, img = cap.read()
         img = detector.find_hands(img)    # count fps
-        lm_list = find_position(img)
-        print(lm_list[1])
+        lm_list = detector.find_position(img)
+        if len(lm_list) != 0:
+            print(lm_list[4])
         # cTime = time.time()
         # fps = 1/(cTime-pTime)
         # pTime = cTime
         # # draw fps on screen 
         # cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,(255,0,255), 3)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
 
 if __name__ == "__main__":
     main()
